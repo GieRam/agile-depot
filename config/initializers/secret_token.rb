82be1bd946fa586df1d0a409a1f2f7bf267b9bf5
Giedrius Ramasauskas <giedrius.ramasauskas@securityicon.com>
2015-04-30 00:28:12 +0300

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AgileDepot::Application.config.secret_key_base = 'ba51271e9fc37553d6fdc8ebc1674ca6ce78e1822dc1238eac1f46573af7cd1a4737d787d8c2cb7e652df1396b7290d6a82527beb18bd7ff6811ab8bbe5531b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AgileDepot::Application.config.session_store :cookie_store, key: '_agile-depot_session'
