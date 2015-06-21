13c753dd8cbed8d624cb5c51c5e802d67e4ad20c
GieRam <gramasauskas@gmail.com>
2015-06-21 11:58:14 +0300

    10.E.Playtime complete, Task F next

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,10 +33,10 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         session[:counter] = 0
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully added' }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
-        format.html { render action: 'new' }
+        format.html { render action: 'new', notice: 'Failed to create line item' }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -50,7 +50,7 @@ class LineItemsController < ApplicationController
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: 'edit' }
+        format.html { render action: 'edit', notice: 'Failed to update line item' }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -61,7 +61,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to @line_item.cart, notice: 'Line item successfully deleted' }
       format.json { head :no_content }
     end
   end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "the truth" do
+    cart = Cart.new
+    assert cart.valid?
+  end
+
+  test "cart line item should save price" do
+    cart = carts(:cart1)
+    [:ruby, :one].each do |p|
+      product = products(p)
+      cart.add_product(product.id).save
+      item = cart.line_items.find_by(product_id: product.id)
+      assert_equal item.price, product.price, "cart line item did not save price"
+    end
+  end
+
+  test "cart line item quantity should increment" do
+    cart = carts(:cart2)
+    product = products(:ruby)
+    multiple = 3
+    multiple.times { cart.add_product(product.id).save }
+    item = cart.line_items.find_by(product_id: product.id)
+    assert_equal multiple, item.quantity, "cart line item quantity is incorrect"
+  end
+
+  test "cart line item price should equal most-recent price" do
+    cart = carts(:cart3)
+    product = products(:ruby)
+    cart.add_product(product.id).save
+    item = cart.line_items.find_by(product_id: product.id)
+    assert_equal product.price, item.price, "line item and product prices do not match"
+  end
+
+  test "cart should be created and destroyed" do
+    cart = Cart.create
+    assert cart.valid?, "cant create cart, Cart.create invalid."
+    assert cart.destroy, "can't destroy cart"
+  end
+
 end
